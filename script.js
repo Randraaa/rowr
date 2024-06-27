034be1702b8fa45ea6faa4e524574ee83c63337f
randrahdyn <28439770-randrahdyn@users.noreply.replit.com>
2024-06-27 06:21:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var numberList= [111,11,1,111,1111,11,11,111,1,111]
+function findNumberLocation(numbers) {
+  for (i=0; i<numbers.length; i++) {
+      if (numbers[i] === 1111) {
+           return i; // Return the index if the number is found
+      }
+  }
+
+}
+
+var numLocation = findNumberLocation(numberList)
+console.log(`1111 is located at index ${numLocation}`)
